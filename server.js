79fc11c30702cdb8ba78566ac42b867e7def84db
Abdur Rahman <abdurr2335@gmail.com>
2023-08-31 11:35:15 +0600

    test

--- server.js
+++ server.js
@@ -2,11 +2,11 @@
 import OpenAI from "openai";
 import cors from "cors";
 import express from "express";
-
+import * as dotenv from "dotenv";
 dotenv.config();
 
 const openai = new OpenAI({
-  apiKey: "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", // This is also the default, can be omitted
+  apiKey: process.env.OpenAI // This is also the default, can be omitted
 });
 const app = express();
 app.use(cors());
